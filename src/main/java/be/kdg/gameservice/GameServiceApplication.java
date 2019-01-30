d32adefceb9464c48ab46726cffec590714c2f9a
Bryan <br.deridder@gmail.com>
2019-01-30 10:53:56 +0100

    Initial commit

---
+++ src/main/java/be/kdg/gameservice/GameServiceApplication.java
@@ -0,0 +1,14 @@
+package be.kdg.gameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GameServiceApplication.class, args);
+    }
+
+}
+

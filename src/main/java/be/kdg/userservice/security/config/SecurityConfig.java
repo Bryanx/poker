d2b3b36c46743d694db2a51ec6fed99d6a2d876b
Rémi <remi.1998@hotmail.com>
2019-02-01 12:37:20 +0100

    Enable CORS for authorization

--- src/main/java/be/kdg/userservice/security/config/SecurityConfig.java
+++ src/main/java/be/kdg/userservice/security/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -44,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/oauth/token").permitAll();
         http.authorizeRequests()
                 .anyRequest().authenticated();
     }

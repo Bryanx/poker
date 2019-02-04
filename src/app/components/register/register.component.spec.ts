87c35ad51ad0c9ae30cd023d34b8ebbc7d289d0f
Rémi Smeets <remi.1998@hotmail.com>
2019-02-04 18:50:20 +0000

    Web client#1 fixes #1

---
+++ src/app/components/register/register.component.spec.ts
@@ -0,0 +1,29 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegisterComponent } from './register.component';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+describe('RegisterComponent', () => {
+  let component: RegisterComponent;
+  let fixture: ComponentFixture<RegisterComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RegisterComponent ],
+      imports: [ FormsModule, ReactiveFormsModule, RouterTestingModule, HttpClientTestingModule ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/model/user.ts
+++ src/app/model/user.ts
@@ -1,5 +1,6 @@
 export class User {
   id: string;
-  userName: string;
+  username: string;
   email: string;
+  password: string;
 }

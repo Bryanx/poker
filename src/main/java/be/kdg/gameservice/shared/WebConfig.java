90379f2cba43359fde5167eb9a09bec636314beb
Jarne <jarne.vanaerde@hotmail.com>
2019-02-05 14:47:00 +0100

    Added documentation, ready to merge.

--- src/main/java/be/kdg/gameservice/shared/WebConfig.java
+++ src/main/java/be/kdg/gameservice/shared/WebConfig.java
@@ -4,6 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * A simple configuration class that is used to create a bean of the modelmapper.
+ */
 @Configuration
 public class WebConfig {
     @Bean

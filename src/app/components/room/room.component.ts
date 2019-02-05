58d0f73847343e74f641bd206084b256d541552c
Anthony Torfs <torfs.anthony@hotmail.com>
2019-02-05 23:54:33 +0100

    Made a room component and basic css grid

---
+++ src/app/components/room/room.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room',
+  templateUrl: './room.component.html',
+  styleUrls: ['./room.component.scss']
+})
+export class RoomComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

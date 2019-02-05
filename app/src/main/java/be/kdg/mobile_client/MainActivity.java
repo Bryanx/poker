38574b9e92df547d91d393cc9e44658fd6e6ea25
Bryan <br.deridder@gmail.com>
2019-02-05 18:11:46 +0100

    Added homescreen

--- app/src/main/java/be/kdg/mobile_client/MainActivity.java
+++ app/src/main/java/be/kdg/mobile_client/MainActivity.java
@@ -1,13 +1,39 @@
 package be.kdg.mobile_client;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.ivLogo) ImageView ivLogo;
+    @BindView(R.id.btnLogin) Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initialiseView();
+    }
+
+    private void initialiseView() {
+        Picasso.get()
+                .load(R.drawable.logo_white)
+                .resize(700, 400)
+                .centerInside()
+                .into(this.ivLogo);
+        btnLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+            startActivity(intent);
+        });
     }
 }

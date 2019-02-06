63ec6b2ddc40ffac5bc35936a6417d803b2042da
Anthony Torfs <torfs.anthony@hotmail.com>
2019-02-06 22:19:27 +0100

    added slider for raise

--- src/app/components/actionbar/actionbar.component.ts
+++ src/app/components/actionbar/actionbar.component.ts
@@ -7,9 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActionbarComponent implements OnInit {
 
+  myModel = 0;
   constructor() { }
 
   ngOnInit() {
   }
 
+  formatLabel(value: number | null) {
+    if (!value) {
+      return 0;
+    }
+
+    if (value >= 1000) {
+      return Math.round(value / 1000) + 'k';
+    }
+
+    return value;
+  }
 }

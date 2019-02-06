b8ddad50a67466b471577c59246747b320cec2cf
Maarten Jorens <jorensmaarten@gmail.com>
2019-02-06 15:22:24 +0100

    Update package.json with local script updateUser method in UserService

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
 import {User} from '../model/user';
+import {catchError} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  url = 'https://poker-user-service.herokuapp.com/api/user';
-  // url = 'http://localhost:8080/api/user';
+  //url = 'https://poker-user-service.herokuapp.com/api/user';
+  url = 'http://localhost:5000/api/user';
 
   constructor(private http: HttpClient) {
   }
@@ -19,5 +20,21 @@ export class UserService {
 
   addUser(user: User): Observable<User> {
     return this.http.post<User>(this.url, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    console.log('Update User in service');
+    return this.http.put(this.url, user).pipe(
+      catchError(this.handleError<any>('updateRoom'))
+    );
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      console.error(error); // log to console
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }

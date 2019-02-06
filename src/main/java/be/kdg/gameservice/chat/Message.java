c06e1906823a4a84ec9c9e68e4335ab7aa30031c
Bryan <br.deridder@gmail.com>
2019-02-06 16:43:02 +0100

    Added websocket config for chat messages

---
+++ src/main/java/be/kdg/gameservice/chat/Message.java
@@ -0,0 +1,13 @@
+package be.kdg.gameservice.chat;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private String name;
+    private String content;
+}

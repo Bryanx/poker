aca3144ea6737be1ffd9582bad4dce4c141f4e1d
Rémi <remi.1998@hotmail.com>
2019-02-06 20:55:28 +0100

    You can now generate a token without needing a password for a certain user 🔥

--- src/main/java/be/kdg/userservice/user/controller/UserController.java
+++ src/main/java/be/kdg/userservice/user/controller/UserController.java
@@ -1,7 +1,9 @@
 package be.kdg.userservice.user.controller;
 
 
+import be.kdg.userservice.security.model.CustomUserDetails;
 import be.kdg.userservice.user.dto.AuthDto;
+import be.kdg.userservice.user.dto.TokenDto;
 import be.kdg.userservice.user.dto.UserDto;
 import be.kdg.userservice.user.exception.UserException;
 import be.kdg.userservice.user.model.User;
@@ -10,24 +12,32 @@ import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Request;
 import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.provider.token.AuthorizationServerTokenServices;
 import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Map;
+import java.util.*;
 
 
 @RestController
 @RequestMapping("/api")
 public class UserController {
-    private final ResourceServerTokenServices tokenServices;
+    private final ResourceServerTokenServices resourceTokenServices;
+    private final AuthorizationServerTokenServices authorizationServerTokenServices;
     private final UserService userServiceImpl;
     private final ModelMapper modelMapper;
 
-    public UserController(ResourceServerTokenServices tokenServices, UserService userServiceImpl, ModelMapper modelMapper) {
-        this.tokenServices = tokenServices;
+    public UserController(ResourceServerTokenServices resourceTokenServices, AuthorizationServerTokenServices authorizationServerTokenServices, UserService userServiceImpl, ModelMapper modelMapper) {
+        this.resourceTokenServices = resourceTokenServices;
+        this.authorizationServerTokenServices = authorizationServerTokenServices;
         this.userServiceImpl = userServiceImpl;
         this.modelMapper = modelMapper;
     }
@@ -39,34 +49,31 @@ public class UserController {
     @GetMapping("/user")
     public ResponseEntity<UserDto> getUser(OAuth2Authentication authentication) {
         OAuth2AuthenticationDetails oAuth2AuthenticationDetails = (OAuth2AuthenticationDetails) authentication.getDetails();
-        Map<String, Object> additionalInfo = tokenServices.readAccessToken(oAuth2AuthenticationDetails.getTokenValue()).getAdditionalInformation();
+        Map<String, Object> additionalInfo = resourceTokenServices.readAccessToken(oAuth2AuthenticationDetails.getTokenValue()).getAdditionalInformation();
         User user = userServiceImpl.findUserById(additionalInfo.get("uuid").toString());
         return new ResponseEntity<>(modelMapper.map(user, UserDto.class), HttpStatus.OK);
     }
-
 
     /**
      * Rest endpoint that creates a user and returns a CREATED status code.
      */
     @PostMapping("/user")
-    public ResponseEntity<UserDto> addUser(@Valid @RequestBody AuthDto authDto) throws UserException {
+    public ResponseEntity<TokenDto> addUser(@Valid @RequestBody AuthDto authDto) throws UserException {
         User userIn = modelMapper.map(authDto, User.class);
         User userOut = userServiceImpl.addUser(userIn);
 
-        return new ResponseEntity<>(modelMapper.map(userOut, UserDto.class), HttpStatus.CREATED);
+        return new ResponseEntity<>(getBearerToken(userOut), HttpStatus.CREATED);
     }
 
-
     /**
-     * Rest endpoint that updates a user and returns an OK status code.
+     * Rest endpoint that updates a user and returns a new JWT token with OK status code.
      */
     @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/user")
-    public ResponseEntity<UserDto> changeUser(@Valid @RequestBody UserDto userDto) throws UserException {
+    public ResponseEntity<TokenDto> changeUser(@Valid @RequestBody UserDto userDto) throws UserException {
         User userIn = modelMapper.map(userDto, User.class);
         User userOut = userServiceImpl.changeUser(userIn);
-
-        return new ResponseEntity<>(modelMapper.map(userOut, UserDto.class), HttpStatus.OK);
+        return new ResponseEntity<>(getBearerToken(userOut), HttpStatus.OK);
     }
 
     /**
@@ -79,5 +86,40 @@ public class UserController {
         User userOut = userServiceImpl.changePassword(userIn);
 
         return new ResponseEntity<>(modelMapper.map(userOut, UserDto.class), HttpStatus.OK);
+    }
+
+    /**
+     * Helper method that lets us generate JWT token with only the username.
+     */
+    private TokenDto getBearerToken(User user) {
+        HashMap<String, String> authorizationParameters = new HashMap<>();
+        authorizationParameters.put("scope", "read");
+        authorizationParameters.put("username", user.getUsername());
+        authorizationParameters.put("client_id", "my-trusted-client");
+        authorizationParameters.put("grant", "password");
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        List<String> roles = new ArrayList<>();
+        roles.add("ROLE_USER");
+
+        Set<String> responseType = new HashSet<>();
+        responseType.add("password");
+
+        Set<String> scopes = new HashSet<>();
+        scopes.add("read");
+        scopes.add("write");
+
+        OAuth2Request authorizationRequest = new OAuth2Request(authorizationParameters, "my-trusted-client", authorities, true,scopes, null, "", responseType, null);
+
+        CustomUserDetails userPrincipal = new CustomUserDetails(user, roles);
+
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userPrincipal, null, authorities);
+
+        OAuth2Authentication authenticationRequest = new OAuth2Authentication(authorizationRequest, authenticationToken);
+        authenticationRequest.setAuthenticated(true);
+
+        OAuth2AccessToken accessToken = authorizationServerTokenServices.createAccessToken(authenticationRequest);
+        return new TokenDto(accessToken.getValue(), accessToken.getExpiresIn());
     }
 }

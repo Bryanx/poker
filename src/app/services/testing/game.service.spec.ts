2aa5ec59a6d2687b24f4bbc92fd62659651215d8
Jarne <jarne.vanaerde@hotmail.com>
2019-02-07 11:08:21 +0100

    Added testing modules to game service.

--- src/app/services/testing/game.service.spec.ts
+++ src/app/services/testing/game.service.spec.ts
@@ -1,9 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { GameService } from '../game.service';
+import {GameService} from '../game.service';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('GameService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: GameService = TestBed.get(GameService);

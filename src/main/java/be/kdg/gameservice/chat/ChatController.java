49bc833f0e5bd5bb0c602de17050ace0e10117ac
Jarne <jarne.vanaerde@hotmail.com>
2019-02-07 21:40:00 +0100

    Cleaned op the mess of bryan :stuck_out_tongue:

--- src/main/java/be/kdg/gameservice/chat/ChatController.java
+++ src/main/java/be/kdg/gameservice/chat/ChatController.java
@@ -4,13 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.messaging.simp.annotation.SubscribeMapping;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
-public class ChatController {
-
+public final class ChatController {
     private final SimpMessagingTemplate template;
 
     /**
@@ -18,8 +16,7 @@ public class ChatController {
      * The message is then send to all players in the same room.
      */
     @MessageMapping("/chatroom/send/{roomId}")
-    public void onReceiveMessage(Message message, @DestinationVariable("roomId") String roomId) {
+    public void onReceiveMessage(MessageDTO message, @DestinationVariable("roomId") String roomId) {
         this.template.convertAndSend("/chatroom/receive/" + roomId, message);
     }
-
 }
--- src/main/java/be/kdg/gameservice/shared/SimpleCORSConfig.java
+++ src/main/java/be/kdg/gameservice/shared/SimpleCORSConfig.java
@@ -20,11 +20,7 @@ public class SimpleCORSConfig implements Filter {
     public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
-        if (response.containsHeader("Origin")) {
-            response.setHeader("Access-Control-Allow-Origin", response.getHeader("Origin"));
-        } else {
-            response.setHeader("Access-Control-Allow-Origin", "*");
-        }
+        response.setHeader("Access-Control-Allow-Origin", "*");
         String options = HttpMethod.OPTIONS.toString();
 
         if (options.equalsIgnoreCase(request.getMethod())) {

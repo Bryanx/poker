3078e912cdb6fa7d2ffc6ec2789ee9f17d0559c5
Bryan <br.deridder@gmail.com>
2019-02-09 23:13:06 +0100

    Styled chat

--- app/src/main/java/be/kdg/mobile_client/activities/RoomActivity.java
+++ app/src/main/java/be/kdg/mobile_client/activities/RoomActivity.java
@@ -1,15 +1,19 @@
 package be.kdg.mobile_client.activities;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.google.gson.Gson;
+
+import java.util.ArrayList;
 
 import javax.inject.Inject;
 
 import be.kdg.mobile_client.R;
+import be.kdg.mobile_client.adapters.MessageAdapter;
 import be.kdg.mobile_client.model.Message;
 import be.kdg.mobile_client.services.ChatService;
 import butterknife.BindView;
@@ -18,10 +22,11 @@ import butterknife.ButterKnife;
 public class RoomActivity extends BaseActivity {
     @BindView(R.id.btnSend) Button btnSend;
     @BindView(R.id.etMessage) EditText etMessage;
-    @BindView(R.id.tvChat) TextView tvChat;
+    @BindView(R.id.lvChat) ListView lvChat;
     @Inject ChatService chatService;
     @Inject Gson gson;
 
+    MessageAdapter messageAdapter;
     private String playerName = "Lotte";
     private int roomNumber = 1;
 
@@ -29,8 +34,10 @@ public class RoomActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         getControllerComponent().inject(this);
         super.onCreate(savedInstanceState);
+        messageAdapter = new MessageAdapter(this, playerName);
         setContentView(R.layout.activity_room);
         ButterKnife.bind(this);
+        lvChat.setAdapter(messageAdapter);
         chatService.connect(roomNumber, playerName);
         addEventHandlers();
     }
@@ -38,7 +45,7 @@ public class RoomActivity extends BaseActivity {
     private void addEventHandlers() {
         chatService.setOnIncomingMessage(msg -> {
             Message message = gson.fromJson(msg.getPayload(), Message.class);
-            tvChat.setText(tvChat.getText() + "\n" + message.getName() + ": " + message.getContent());
+            runOnUiThread(() -> messageAdapter.add(message));
         });
         btnSend.setOnClickListener(e -> {
             if (etMessage.getText().length() > 0) {

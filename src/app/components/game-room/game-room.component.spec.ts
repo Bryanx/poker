80d62ea1cc44ee6fff99fc1f133a522b88c223f2
Jarne <jarne.vanaerde@hotmail.com>
2019-02-10 14:20:33 +0100

    fixed all tests :fire:

--- src/app/components/game-room/game-room.component.spec.ts
+++ src/app/components/game-room/game-room.component.spec.ts
@@ -1,10 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { GameRoomComponent } from './game-room.component';
+import {GameRoomComponent} from './game-room.component';
 import {PlayerComponent} from '../player/player.component';
 import {ChatComponent} from '../chat/chat.component';
 import {GameTableComponent} from '../game-table/game-table.component';
 import {ActionbarComponent} from '../actionbar/actionbar.component';
+import {FormsModule} from '@angular/forms';
+import {CardComponent} from '../card/card.component';
+import {MatSlider} from '@angular/material';
+import {RouterTestingModule} from '@angular/router/testing';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
 
 describe('GameRoomComponent', () => {
   let component: GameRoomComponent;
@@ -12,15 +17,10 @@ describe('GameRoomComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        GameRoomComponent,
-        PlayerComponent,
-        ChatComponent,
-        GameTableComponent,
-        ActionbarComponent
-      ]
+      declarations: [GameRoomComponent, PlayerComponent, ChatComponent, GameTableComponent, ActionbarComponent, CardComponent, MatSlider],
+      imports: [RouterTestingModule, HttpClientTestingModule, FormsModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {

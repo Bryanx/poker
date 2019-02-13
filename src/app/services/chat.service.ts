edd9c64b6983ad73dae6e3b8f763bb47dfb2555e
Jarne <jarne.vanaerde@hotmail.com>
2019-02-13 09:21:39 +0100

    Update chat.service.ts

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -15,6 +15,7 @@ export class ChatService {
     this.server = Stomp.over(new WebSocket(this.serverUrl));
     this.server.heartbeat.outgoing = 10000; // send heartbeats every 10000ms
     this.server.heartbeat.incoming = 10000;
+    this.server.debug = null;
     return this.server;
   }
 

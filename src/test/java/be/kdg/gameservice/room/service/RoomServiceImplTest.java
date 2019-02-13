0de58171c8ed2141dac7e57177e6857a14c8e94b
Jarne <jarne.vanaerde@hotmail.com>
2019-02-13 15:34:10 +0100

    Working on test
    *** WARNING ***
    Tests could be unstable at this point, do not use them for testing your code.

--- src/test/java/be/kdg/gameservice/room/service/RoomServiceImplTest.java
+++ src/test/java/be/kdg/gameservice/room/service/RoomServiceImplTest.java
@@ -1,17 +1,44 @@
 package be.kdg.gameservice.room.service;
 
-import be.kdg.gameservice.UtilTesting;
-import be.kdg.gameservice.room.service.impl.RoomServiceImpl;
+import be.kdg.gameservice.room.model.GameRules;
+import be.kdg.gameservice.room.model.Room;
+import be.kdg.gameservice.room.persistence.RoomRepository;
+import be.kdg.gameservice.room.service.api.RoomService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.transaction.Transactional;
+
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
-public class RoomServiceImplTest extends UtilTesting {
+@Transactional
+public class RoomServiceImplTest {
+    @Autowired
+    private RoomService roomService;
+    @Autowired
+    private RoomRepository roomRepository;
+
+    @Before
+    public void setup() {
+        roomRepository.deleteAll();
+
+        Room room1 = new Room(GameRules.TEXAS_HOLD_EM, "test room 1");
+        Room room2 = new Room(GameRules.TEXAS_HOLD_EM, "test room 2");
+        Room room3 = new Room(GameRules.TEXAS_HOLD_EM_DIFFICULT, "test room 3");
+
+        roomRepository.save(room1);
+        roomRepository.save(room2);
+        roomRepository.save(room3);
+    }
+
     @Test
-    public void testImmutabilityAttributes() {
-        testImmutabilityAttributes(RoomServiceImpl.class);
+    public void getRooms() {
+        assertEquals(3, roomService.getRooms().size());
     }
 }
--- src/test/java/be/kdg/gameservice/round/model/DeckTest.java
+++ src/test/java/be/kdg/gameservice/round/model/DeckTest.java
@@ -1,44 +1,17 @@
 package be.kdg.gameservice.round.model;
 
-import be.kdg.gameservice.card.Card;
-import be.kdg.gameservice.card.CardType;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public final class DeckTest {
-    private Deck deck;
-
-    @Before
-    public void setup() {
-        this.deck = new Deck();
-    }
-
     @Test
     public void testCardCount() {
-       assertEquals(52, deck.getNumberOfCards());
+       assertEquals(52, new Deck().getNumberOfCards());
     }
-
-    @Test
-    public void testShuffleDeck() {
-        List<Boolean> samePlaces = new ArrayList<>();
-
-        for (CardType card : CardType.values()) {
-            Card deckCard = deck.getCard();
-            samePlaces.add(card.equals(deckCard.type));
-        }
-
-        assertNotEquals(52, samePlaces.stream().filter(p -> p).count());
-    }
-
 }

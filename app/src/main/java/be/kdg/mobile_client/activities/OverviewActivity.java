ee8c51a8f024149cb2c7ac64d9c043ba560861fc
Jarne <jarne.vanaerde@hotmail.com>
2019-02-16 22:25:54 +0100

    Added acitvity overview template

---
+++ app/src/main/java/be/kdg/mobile_client/activities/OverviewActivity.java
@@ -0,0 +1,15 @@
+package be.kdg.mobile_client.activities;
+
+import android.os.Bundle;
+
+import be.kdg.mobile_client.R;
+import butterknife.ButterKnife;
+
+public class OverviewActivity extends BaseActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

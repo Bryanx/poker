a74f6a41d17fd537b4a5acae7bf67fb6a3786eec
Bryan <br.deridder@gmail.com>
2019-02-18 21:05:52 +0100

    Added wrapper for callbacks

---
+++ app/src/main/java/be/kdg/mobile_client/services/CallbackWrapper.java
@@ -0,0 +1,27 @@
+package be.kdg.mobile_client.services;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CallbackWrapper<T> implements Callback<T> {
+    private Wrapper<T> wrapper;
+
+    public CallbackWrapper(Wrapper<T> wrapper) {
+        this.wrapper = wrapper;
+    }
+
+    @Override
+    public void onFailure(Call<T> call, Throwable t) {
+        wrapper.onResult(t, null);
+    }
+
+    @Override
+    public void onResponse(Call<T> call, Response<T> response) {
+        wrapper.onResult(null, response);
+    }
+
+    public static interface Wrapper<T> {
+        void onResult(Throwable t, Response<T> response);
+    }
+}

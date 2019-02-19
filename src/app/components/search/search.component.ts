35aa787a74d1ced40a68fb6931ed1c84060c5627
Jarne <jarne.vanaerde@hotmail.com>
2019-02-19 08:27:47 +0100

    Added docs.

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -5,6 +5,10 @@ import {Subject} from 'rxjs';
 import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
 import {DomSanitizer} from '@angular/platform-browser';
 
+/**
+ * This component will be used for searching through all the users
+ * that are in the system.
+ */
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
@@ -19,6 +23,11 @@ export class SearchComponent implements OnInit {
   constructor(private userService: UserService, private sanitizer: DomSanitizer) {
   }
 
+  /**
+   * When the component is created, the functionality will be piped into the subject and be subscribed on.
+   * A subject is an observable stream were you can put data inside it and manipulate it.
+   * This subject is defined as a string, which means that the stream will only accept strings as input.
+   */
   ngOnInit(): void {
     this.subject.pipe(
       debounceTime(this.debounceTime as number),
@@ -27,6 +36,12 @@ export class SearchComponent implements OnInit {
     this.getUsers();
   }
 
+  /**
+   * Gives back the profile picture of the users. If there no picture, a null
+   * value will be returned.
+   *
+   * @param user The user with the possible picture.
+   */
   getProfilePicture(user: User) {
     if (user.profilePicture === null) {
       return user.profilePictureSocial;
@@ -35,14 +50,27 @@ export class SearchComponent implements OnInit {
     }
   }
 
+  /**
+   * Gives back the first letter in upper case as initial of the user.
+   *
+   * @param user The user we need to get the initials from.
+   */
   getInitials(user: User): string {
     return user.username.charAt(0).toUpperCase();
   }
 
-  changeSubject(): void {
+  /**
+   * Adds the input string that is two-way-bind to the input-field to the subject.
+   */
+  addToSubject(): void {
     this.subject.next(this.inputString);
   }
 
+  /**
+   * Gets all new users based on the input string that was buffered in the subject stream.
+   *
+   * @param input The input string that will be used to request the users
+   */
   private getUsers(input: string = ''): void {
     if (input === '') {
       this.userService.getUsers().subscribe(users => this.users = users);
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -5,6 +5,9 @@ import {User} from '../model/user';
 import {catchError} from 'rxjs/operators';
 import {AuthResult} from '../model/authResult';
 
+/**
+ * This service is used to make API calls to the user micro service backend.
+ */
 @Injectable({
   providedIn: 'root'
 })
@@ -15,30 +18,62 @@ export class UserService {
   constructor(private http: HttpClient) {
   }
 
+  /**
+   * Gives back your own credentials.
+   */
   getMyself(): Observable<User> {
     return this.http.get<User>(this.url);
   }
 
+  /**
+   * Returns the requested user.
+   *
+   * @param userId The id of the requested user.
+   */
   getUser(userId: string): Observable<User> {
     return this.http.get<User>(this.url + '/' + userId);
   }
 
+  /**
+   * Returns all the users that are in the system.
+   */
   getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.url + 's');
   }
 
+  /**
+   * Gives back all the users that have the name-string inside
+   * their name.
+   *
+   * @param name The name of the users.
+   */
   getUsersByName(name: string): Observable<User[]> {
     return this.http.get<User[]>(this.url + 's/' + name);
   }
 
+  /**
+   * Adds a user to the system. Usually called on when a users registers.
+   *
+   * @param user The newly registered user.
+   */
   addUser(user: User): Observable<AuthResult> {
     return this.http.post<AuthResult>(this.url, user);
   }
 
+  /**
+   * Changes one or more attributes of the user except the password.
+   *
+   * @param user The user that needs to be changed.
+   */
   changeUser(user: User): Observable<AuthResult> {
     return this.http.put<AuthResult>(this.url, user);
   }
 
+  /**
+   * Changes the password of the given user.
+   *
+   * @param user The user with the new password.
+   */
   changePassword(user: any): Observable<AuthResult> {
     return this.http.patch<any>(this.url, user);
   }

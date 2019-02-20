828149ac61bd3c1866f092d3df23ad2fe02fe3e4
Jarne <jarne.vanaerde@hotmail.com>
2019-02-20 11:43:31 +0100

    Added recycler adapter for users

---
+++ app/src/main/java/be/kdg/mobile_client/adapters/UserRecyclerAdapter.java
@@ -0,0 +1,73 @@
+package be.kdg.mobile_client.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import be.kdg.mobile_client.R;
+import be.kdg.mobile_client.model.User;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class UserRecyclerAdapter extends RecyclerView.Adapter<UserRecyclerAdapter.ViewHolder> {
+    private List<User> users;
+
+    /**
+     * Inflates the layout that will be used to display each user.
+     *
+     * @return a ViewHolder that is based on the inflated view.
+     */
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_room, parent, false);
+        return new UserRecyclerAdapter.ViewHolder(view);
+    }
+
+    /**
+     * Binds all the information of one user to one of the view holders.
+     *
+     * @param holder   The holder that "holds" the views that are created so they can be recycled.
+     * @param position The position in the array.
+     */
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        User user = users.get(position);
+
+        holder.tvName.setText(user.getUsername());
+        Picasso.get()
+                .load(R.drawable.pic_placeholder)
+                .resize(35, 35)
+                .centerInside()
+                .transform(new CircleTransformation())
+                .into(holder.ivPic);
+    }
+
+    @Override
+    public int getItemCount() {
+        return users.size();
+    }
+
+    /**
+     * This inner class will hold the layouts in memory so that
+     * they can be recycled.
+     */
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvName;
+        ImageView ivPic;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvName = itemView.findViewById(R.id.tvUserName);
+            ivPic = itemView.findViewById(R.id.ivUserPic);
+        }
+    }
+}

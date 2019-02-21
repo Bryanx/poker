a997e91a39cc4cb2fe851dc0f2b82d55fd0b2d8a
Anthony Torfs <torfs.anthony@hotmail.com>
2019-02-21 14:23:04 +0100

    fixed border and test

--- src/app/components/game-room-admin/game-room-admin.component.spec.ts
+++ src/app/components/game-room-admin/game-room-admin.component.spec.ts
@@ -1,6 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GameRoomAdminComponent } from './game-room-admin.component';
+import {GameRoomComponent} from '../game-room/game-room.component';
+import {PlayerComponent} from '../player/player.component';
+import {ChatComponent} from '../chat/chat.component';
+import {GameTableComponent} from '../game-table/game-table.component';
+import {ActionbarComponent} from '../actionbar/actionbar.component';
+import {CardComponent} from '../card/card.component';
+import {MatSlider} from '@angular/material';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {RouterTestingModule} from '@angular/router/testing';
+import {TranslatePipe} from '../../translate.pipe';
 
 describe('GameRoomAdminComponent', () => {
   let component: GameRoomAdminComponent;
@@ -8,7 +19,16 @@ describe('GameRoomAdminComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GameRoomAdminComponent ]
+      declarations: [
+        GameRoomAdminComponent,
+        TranslatePipe
+      ],
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule,
+        ReactiveFormsModule
+      ]
     })
     .compileComponents();
   }));

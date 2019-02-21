769a739cc8690432a6674e759ba2e58f90ad2ecf
Rémi <remi.1998@hotmail.com>
2019-02-21 22:50:43 +0100

    Cards are now displayed according to fase

--- src/app/components/game-table/game-table.component.ts
+++ src/app/components/game-table/game-table.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Round} from '../../model/round';
+import {Phase} from '../../model/phase';
 
 @Component({
   selector: 'app-game-table',
@@ -6,10 +8,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-table.component.scss']
 })
 export class GameTableComponent implements OnInit {
+  _round: Round;
+  firstCard: boolean;
+  secondCard: boolean;
+  thirthCard: boolean;
+  fourthCard: boolean;
+  fifthCard: boolean;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  @Input() set round(round: Round) {
+    this._round = round;
+
+    if (this._round) {
+      this.checkCards();
+    }
+  }
+
+  checkCards() {
+    switch (this._round.currentPhase) {
+      case Phase.Flop:
+        this.firstCard = true;
+        this.secondCard = true;
+        this.thirthCard = true;
+        break;
+      case Phase.Turn:
+        this.firstCard = true;
+        this.secondCard = true;
+        this.thirthCard = true;
+        this.fourthCard = true;
+        break;
+      case Phase.River:
+        this.firstCard = true;
+        this.secondCard = true;
+        this.thirthCard = true;
+        this.fourthCard = true;
+        this.fifthCard = true;
+        break;
+      case Phase.Showdown:
+        this.firstCard = true;
+        this.secondCard = true;
+        this.thirthCard = true;
+        this.fourthCard = true;
+        this.fifthCard = true;
+        break;
+      default:
+        this.firstCard = false;
+        this.secondCard = false;
+        this.thirthCard = false;
+        this.fourthCard = false;
+        this.fifthCard = false;
+    }
+  }
 }

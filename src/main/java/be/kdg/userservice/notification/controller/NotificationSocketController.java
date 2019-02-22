fca7f59c7dcb9e323c2c823127bbaba8f953b3b0
Jarne <jarne.vanaerde@hotmail.com>
2019-02-22 14:14:22 +0100

    Added socket api

--- src/main/java/be/kdg/userservice/notification/controller/NotificationSocketController.java
+++ src/main/java/be/kdg/userservice/notification/controller/NotificationSocketController.java
@@ -1,17 +1,29 @@
 package be.kdg.userservice.notification.controller;
 
+import be.kdg.userservice.notification.controller.dto.NotificationDTO;
+import be.kdg.userservice.notification.model.Notification;
+import be.kdg.userservice.notification.service.api.NotificationService;
+import be.kdg.userservice.user.exception.UserException;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 public class NotificationSocketController {
     private final SimpMessagingTemplate template;
+    private final ModelMapper modelMapper;
+    private final NotificationService notificationService;
 
-    @MessageMapping("/user/")
-    public void sendNotification() {
-
+    @MessageMapping("/user/{receiverId}/send-friend-request")
+    public void sendFriendRequest(@DestinationVariable("receiverId") String receiverId, @Valid NotificationDTO notificationDTO) throws UserException {
+        Notification notificationIn = notificationService.addNotification(receiverId, notificationDTO.getSender().getId(), notificationDTO.getMessage(), notificationDTO.getType());
+        NotificationDTO notificationOut = modelMapper.map(notificationIn, NotificationDTO.class);
+        this.template.convertAndSend("/user/receive-notification/" + receiverId, notificationOut);
     }
 }
--- src/main/java/be/kdg/userservice/notification/controller/dto/NotificationDTO.java
+++ src/main/java/be/kdg/userservice/notification/controller/dto/NotificationDTO.java
@@ -1,8 +1,20 @@
 package be.kdg.userservice.notification.controller.dto;
 
+import be.kdg.userservice.notification.model.NotificationType;
+import be.kdg.userservice.user.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.Valid;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NotificationDTO {
+    private int id;
+    @Valid
+    private User sender;
+    private String message;
+    private NotificationType type;
 }

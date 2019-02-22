a4b398075969b73b72578a5ba73cbbc78c3dcff8
Bryan <br.deridder@gmail.com>
2019-02-22 15:52:17 +0100

    Added navigation to user account from search

--- app/src/main/java/be/kdg/mobile_client/adapters/UserRecyclerAdapter.java
+++ app/src/main/java/be/kdg/mobile_client/adapters/UserRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package be.kdg.mobile_client.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,7 @@ import java.util.List;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import be.kdg.mobile_client.R;
+import be.kdg.mobile_client.activities.AccountActivity;
 import be.kdg.mobile_client.activities.UserSearchActivity;
 import be.kdg.mobile_client.model.User;
 import lombok.AllArgsConstructor;
@@ -47,6 +49,11 @@ public class UserRecyclerAdapter extends RecyclerView.Adapter<UserRecyclerAdapte
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         User user = users.get(position);
         holder.tvName.setText(user.getUsername());
+        holder.tvName.setOnClickListener(e -> {
+            Intent intent = new Intent(ctx, AccountActivity.class);
+            intent.putExtra(ctx.getString(R.string.userid), user.getId());
+            ctx.startActivity(intent);
+        });
         holder.btnAdd.setOnClickListener(e -> {
             if (ctx instanceof  UserSearchActivity) {
                 ((UserSearchActivity) ctx).addFriend(user);

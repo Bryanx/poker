aedb385e7e8def91d5bf998ff5c05d9398475ac0
Jarne <jarne.vanaerde@hotmail.com>
2019-02-22 11:58:00 +0100

    Fixed .spec files

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,9 +1,11 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SearchComponent } from './search.component';
+import {SearchComponent} from './search.component';
 import {FormsModule, NgModel, ReactiveFormsModule} from '@angular/forms';
 import {RouterLink} from '@angular/router';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {TranslatePipe} from '../../translate.pipe';
+import {RouterTestingModule} from '@angular/router/testing';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
@@ -11,10 +13,10 @@ describe('SearchComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SearchComponent, NgModel, RouterLink ],
-      imports: [ HttpClientTestingModule, ReactiveFormsModule  ]
+      declarations: [SearchComponent, NgModel, TranslatePipe],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, RouterTestingModule]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {

e762961185090ea49eb691fa83a6600ad1db0494
Anthony Torfs <torfs.anthony@hotmail.com>
2019-02-25 13:42:16 +0100

    fix test

--- src/app/services/authorization.service.ts
+++ src/app/services/authorization.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import * as moment from 'moment';
 import {Auth} from '../model/auth';
@@ -51,7 +51,10 @@ export class AuthorizationService {
   }
 
   isAdmin() {
+    if (localStorage.getItem('jwt_token')) {
       return this.helper.decodeToken(localStorage.getItem('jwt_token')).role === 'ROLE_ADMIN';
+    }
+    return false;
   }
 
   socialLogin(user: User) {

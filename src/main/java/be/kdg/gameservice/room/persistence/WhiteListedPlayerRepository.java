b175d35a988244cda5d3aa9b3f7c6da2ef6c5988
Jarne <jarne.vanaerde@hotmail.com>
2019-02-26 14:30:00 +0100

    Update WhiteListedPlayerRepository.java

--- src/main/java/be/kdg/gameservice/room/persistence/WhiteListedPlayerRepository.java
+++ src/main/java/be/kdg/gameservice/room/persistence/WhiteListedPlayerRepository.java
@@ -1,9 +1,9 @@
 package be.kdg.gameservice.room.persistence;
 
-import be.kdg.gameservice.room.model.WhiteListedPlayer;
+import be.kdg.gameservice.room.model.WhiteListedUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface WhiteListedPlayerRepository extends JpaRepository<WhiteListedPlayer, Integer> {
+public interface WhiteListedPlayerRepository extends JpaRepository<WhiteListedUser, Integer> {
 }

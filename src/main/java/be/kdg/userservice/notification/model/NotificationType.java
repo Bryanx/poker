56bf4fe68a5708acd470328cc93811df33bf6c92
Jarne <jarne.vanaerde@hotmail.com>
2019-03-01 15:10:33 +0100

    Update NotificationType.java

--- src/main/java/be/kdg/userservice/notification/model/NotificationType.java
+++ src/main/java/be/kdg/userservice/notification/model/NotificationType.java
@@ -4,5 +4,5 @@ package be.kdg.userservice.notification.model;
  * All the types of notifications that a user can receive.
  */
 public enum NotificationType {
-    FRIEND_REQUEST, GAME_REQUEST, GLOBAL_MESSAGE
+    FRIEND_REQUEST, ADD_PRIVATE_ROOM, DELETE_PRIVATE_ROOM, GAME_REQUEST, GLOBAL_MESSAGE
 }

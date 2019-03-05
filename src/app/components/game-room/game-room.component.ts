a08f0d7278181ae4bcf5c80ed5b995cb431cea84
Jarne <jarne.vanaerde@hotmail.com>
2019-03-05 16:03:15 +0100

    Update game-room.component.ts

--- src/app/components/game-room/game-room.component.ts
+++ src/app/components/game-room/game-room.component.ts
@@ -15,6 +15,8 @@ import {UserService} from '../../services/user.service';
 import {Location} from '@angular/common';
 import {WebSocketService} from '../../services/web-socket.service';
 import {HomeVisibleService} from '../../services/home-visible.service';
+import {forkJoin} from 'rxjs';
+import {User} from '../../model/user';
 
 @Component({
   selector: 'app-room',
@@ -42,7 +44,7 @@ export class GameRoomComponent implements OnInit, OnDestroy {
     this.homeObservable.emitNewState(true);
     const roomId = this.curRouter.snapshot.paramMap.get('id') as unknown;
 
-    this.getRoom(roomId as number);
+    this.getData(roomId as number);
 
     this.joinRoomInterval = setInterval(() => {
       if (this.room.gameRules.id !== 0) {
@@ -126,12 +128,20 @@ export class GameRoomComponent implements OnInit, OnDestroy {
     });
   }
 
-  getRoom(id: number): void {
-    this.roomService.getRoom(id).subscribe(room => {
-      this.room = room as Room;
+  getData(id: number): void {
+    const getRoom = this.roomService.getRoom(id);
+    const getMyself = this.userService.getMyself();
 
+    forkJoin(getRoom, getMyself).subscribe(bundle => {
+      console.log(bundle);
+      this.room = bundle[0] as Room;
       if (this.room.playersInRoom.length >= this.room.gameRules.maxPlayerCount) {
         this.navigateBack();
+      }
+
+      const me = bundle[1] as User;
+      if (me.level > this.room.gameRules.maxLevel || me.level < this.room.gameRules.minLevel) {
+        this.location.back();
       }
     });
   }

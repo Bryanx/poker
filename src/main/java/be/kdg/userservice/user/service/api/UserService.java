53b7c4bd7cd1f4d2c8ae5685e729e1846bfe19d4
Jarne Van Aerde <jarne.vanaerde@student.kdg.be>
2019-03-08 15:01:17 +0000

    Apply suggestion to src/main/java/be/kdg/userservice/user/service/api/UserService.java

--- src/main/java/be/kdg/userservice/user/service/api/UserService.java
+++ src/main/java/be/kdg/userservice/user/service/api/UserService.java
@@ -3,7 +3,7 @@ package be.kdg.userservice.user.service.api;
 import be.kdg.userservice.user.exception.UserException;
 import be.kdg.userservice.user.model.User;
 import be.kdg.userservice.user.model.UserRole;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 
 import java.util.List;
 

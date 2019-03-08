dace5dadcc9267f682255bb7b3e4bab90798d9a0
Jarne <jarne.vanaerde@hotmail.com>
2019-03-08 14:10:18 +0100

    final fixes

--- src/app/components/global-message/global-message.component.spec.ts
+++ src/app/components/global-message/global-message.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GlobalMessageComponent } from './global-message.component';
+import {TranslatePipe} from '../../translate.pipe';
+import {NgModel, ReactiveFormsModule} from '@angular/forms';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+import {MatSnackBarModule} from '@angular/material';
 
 describe('GlobalMessageComponent', () => {
   let component: GlobalMessageComponent;
@@ -8,7 +13,8 @@ describe('GlobalMessageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GlobalMessageComponent ]
+      declarations: [ GlobalMessageComponent, TranslatePipe, NgModel ],
+      imports: [ AngularFontAwesomeModule, HttpClientTestingModule, MatSnackBarModule, ReactiveFormsModule ]
     })
     .compileComponents();
   }));

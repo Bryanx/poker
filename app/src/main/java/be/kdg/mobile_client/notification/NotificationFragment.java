6e483ddf3dad264268c8ea3bd495fdc3aac1f68e
Jarne <jarne.vanaerde@hotmail.com>
2019-03-11 18:14:43 +0100

    Revert "Notifications are on fire :fire:"
    This reverts commit 98e954c2063e97678de5e31fb2f740a8f148d52a.

--- app/src/main/java/be/kdg/mobile_client/notification/NotificationFragment.java
+++ app/src/main/java/be/kdg/mobile_client/notification/NotificationFragment.java
@@ -1,21 +1,15 @@
 package be.kdg.mobile_client.notification;
 
-import android.content.res.Configuration;
-import android.media.VolumeShaper;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -40,7 +34,6 @@ public class NotificationFragment extends Fragment {
     @BindView(R.id.progressBar) ProgressBar progressBar;
     @BindView(R.id.tvNoNots) TextView tvNoNots;
     @BindView(R.id.lvNots) RecyclerView lvNots;
-    @BindView(R.id.ivRefresh) ImageView ivRefresh;
     @BindView(R.id.btnDeleteAll) Button btnDeleteAll;
     @Inject NotificationViewModel viewModel;
 
@@ -50,21 +43,9 @@ public class NotificationFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_notification, container, false);
         ButterKnife.bind(this, view);
         getControllerComponent().inject(this);
-        placeImages();
-        addEventListeners();
         getNotifications();
+        addEventListeners();
         return view;
-    }
-
-    /**
-     * Places the images into the fragment
-     */
-    private void placeImages() {
-        Picasso.get()
-                .load(R.drawable.refresh)
-                .resize(30, 30)
-                .centerInside()
-                .into(ivRefresh);
     }
 
     /**
@@ -78,8 +59,6 @@ public class NotificationFragment extends Fragment {
             initializeAdapter(new ArrayList<>()); //refresh data with empty set.
             Toast.makeText(getContext(), "Deleted all notifications", Toast.LENGTH_LONG).show();
         });
-
-        ivRefresh.setOnClickListener(e -> getNotifications());
     }
 
     /**
@@ -96,7 +75,6 @@ public class NotificationFragment extends Fragment {
      * Retrieves the notifications out of the view model.
      */
     private void getNotifications() {
-        progressBar.setVisibility(View.VISIBLE);
         viewModel.getNotifications().observe(this, this::initializeAdapter);
     }
 
@@ -107,12 +85,10 @@ public class NotificationFragment extends Fragment {
      * @param notifications The notifications that need to be used by the adapter.
      */
     private void initializeAdapter(List<Notification> notifications) {
+        progressBar.setVisibility(View.GONE);
         if (notifications.size() == 0) tvNoNots.setVisibility(View.VISIBLE);
-        else tvNoNots.setVisibility(View.GONE);
-
         NotificationRecyclerAdapter adapter = new NotificationRecyclerAdapter(getContext(), notifications, viewModel.getNotificationService());
         lvNots.setAdapter(adapter);
         lvNots.setLayoutManager(new LinearLayoutManager(getContext()));
-        progressBar.setVisibility(View.GONE);
     }
 }

36838fbf2f936470eecc64fe2fdfb248460bb9a4
Jarne <jarne.vanaerde@hotmail.com>
2019-03-17 00:11:44 +0100

    Added navigation to replays

--- src/app/components/replay/replay.component.ts
+++ src/app/components/replay/replay.component.ts
@@ -15,9 +15,16 @@ export class ReplayComponent implements OnInit {
 
   ngOnInit() {
     this.curReplay = 0;
-    this.roomService.getReplays().subscribe(replays => {
-      console.log(replays);
-      this.replays = replays;
-    });
+    this.roomService.getReplays().subscribe(replays => this.replays = replays);
+  }
+
+  switchPage(val: number) {
+    if (this.curReplay + val < 0) {
+      this.curReplay = this.replays.length - 1;
+    } else if (this.curReplay + val > this.replays.length - 1) {
+      this.curReplay = 0;
+    } else {
+      this.curReplay += val;
+    }
   }
 }
